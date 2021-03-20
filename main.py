ca269595956c0fabec20498365d184e0ec4abb13
Fajer <aldousarifajer@gmail.com>
2021-03-21 00:06:54 +0300

    Coordinates Generator

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+
+
+def coordinates_generator():
+    x=list(range(0,101))
+    y=list(range(0,111))
+    z=list(range(0,101))
+
+
 # populating the dictionary for our code.
+
 dicts = {}
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
@@ -7,15 +16,12 @@ print(len(values))
 frequency_range = list(range(10,47))
 code = dict(zip(frequency_range, values))
 
+class Vessel:
+    def __init__(self):
+
 class Loudspeaker:
     def __init__(self):
 
-    def rotation_north:
-    def rotation_east:
-    def rotation_west:
-    def rotation_south:
-
-    __
 class Hydrophone:
     def __init__(self,signal):
         # signal is Boolean
