0484abc9e234f623ceb4523a605596f5af950755
Fajer <aldousarifajer@gmail.com>
2021-03-20 22:42:04 +0300

    Main Code Blueprint

---
+++ main.py
@@ -0,0 +1,25 @@
+# populating the dictionary for our code.
+dicts = {}
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+numbers = ['0','1','2','3','4','5','6','7','8','9']
+values = letters + numbers
+print(len(values))
+frequency_range = list(range(10,47))
+code = dict(zip(frequency_range, values))
+
+class Loudspeaker:
+    def __init__(self):
+
+    def rotation_north:
+    def rotation_east:
+    def rotation_west:
+    def rotation_south:
+
+    __
+class Hydrophone:
+    def __init__(self,signal):
+        # signal is Boolean
+        self.signal=signal
+    
+    
+
