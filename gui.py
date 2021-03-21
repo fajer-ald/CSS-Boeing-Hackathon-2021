3339132ec901f6077ba707ea7621a7bb2f161d8e
Ivan <need-for-speed2009@yandex.ru>
2021-03-21 10:22:27 +0300

    Data integration from main script

--- gui.py
+++ gui.py
@@ -1,21 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-#import main
+import main
 from tqdm.auto import tqdm
 
-vessel_x=30
-vessel_y=14
-vessel_z=40
-freqs=[34,46,50]
-counts=[1,2,3]
+data=main.main_code()
+
+freqs=[10,15,20,34,35,36]
+counts=[1,1,2,3,4,2]
 
 symbols = [10,26,13,28]
 
-pbar1 = tqdm(total=len(symbols), position=0, desc='Sending data')
-pbar2 = tqdm(total=len(symbols), position=1, desc='Receiving data')
+pbar1 = tqdm(total=len(data[0]), position=0, desc='Sending data')
+pbar2 = tqdm(total=len(data[0]), position=1, desc='Receiving data')
 
-for i in range(len(symbols)):
+for i in range(len(data[0])):
     time.sleep(0.5)
     pbar1.update(1)
     time.sleep(0.5)
@@ -24,6 +23,7 @@ for i in range(len(symbols)):
 fig, ax=plt.subplots()
 freqs_g=ax.bar(freqs, counts, 0.8, color='blue')
 ax.set_xlabel('Frequency, Hz')
-ax.set_ylabel('Score')
+ax.set_ylabel('Times transmitted')
+plt.xlim([10, 36])
 ax.set_title('Frequency spectrum of the transmission')
 plt.show()
--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-
-
-
-
-
-# populating the dictionary for our code.
-
-dicts = {}
-letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-numbers = ['0','1','2','3','4','5','6','7','8','9']
-values = letters + numbers
-print(len(values))
-frequency_range = list(range(10,47))
-code = dict(zip(frequency_range, values))
+import random
+import sys
 
 class Vessel:
-    def __init__(self, xcoordinate, ycoordinate, zcoordinate):
-        self.xcoordinate=xcoordinate
-        self.ycoordinate=ycoordinate
-        self.zcoordinate=zcoordinate
-    def coordinates_generator(self,xcoordinate,ycoordinate,zcoordinate):
-        x=list(range(0,101))
-        y=list(range(0,111))
-        z=list(range(0,101))
+    def __init__(self):
+        self.x=0
+        self.y=0
+        self.z=0
+    def gen_coords(self):
+        self.x=random.randrange(0,101)
+        self.y=random.randrange(0,111)
+        self.z=random.randrange(0,101)
 
+def main_code():
+    ship = Vessel()
+    ship.gen_coords()
+    ship.y=0
+    submarine = Vessel()
+    submarine.gen_coords()
+
+    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    numbers = ['0','1','2','3','4','5','6','7','8','9']
+    values = letters + numbers
+    freq = list(range(10,46))
+    code = dict(zip(values,freq))
+    transcript=[]
+
+    msg = input('Please enter your message: ')
+    msg = msg.lower()
+    if msg.isalnum() is False:
+        print('Message not alphanumeric, unable to send')
+        sys.exit(1)
+    for i in range(len(msg)):
+        transcript.append(code.get(msg[i]))
+    print(transcript)
+    return transcript,ship.x,ship.y,ship.z,submarine.x,submarine.y,submarine.z
+		
 """
 class Loudspeaker:
     def __init__(self):
@@ -32,6 +44,3 @@ class Hydrophone:
         # signal is Boolean
         self.signal=signal
   """  
-    
-surface_vessel=Vessel(randrange(),randrange(),100)
-submarine=Vessel(randrange(),randrange(),0)
