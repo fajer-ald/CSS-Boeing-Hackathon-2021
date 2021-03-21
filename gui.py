c7ed144aef0135a5c115086aba52f7f252477859
Ivan <need-for-speed2009@yandex.ru>
2021-03-21 10:33:25 +0300

    Frequency spectrum functionality restore

--- gui.py
+++ gui.py
@@ -6,10 +6,8 @@ from tqdm.auto import tqdm
 
 data=main.main_code()
 
-freqs=[10,15,20,34,35,36]
-counts=[1,1,2,3,4,2]
-
-symbols = [10,26,13,28]
+freqs=list(range(10,46))
+counts=[]
 
 pbar1 = tqdm(total=len(data[0]), position=0, desc='Sending data')
 pbar2 = tqdm(total=len(data[0]), position=1, desc='Receiving data')
@@ -20,10 +18,13 @@ for i in range(len(data[0])):
     time.sleep(0.5)
     pbar2.update(1)
 	
+for i in range(len(freqs)):
+    counts.append(data[0].count(freqs[i]))
+	
 fig, ax=plt.subplots()
 freqs_g=ax.bar(freqs, counts, 0.8, color='blue')
 ax.set_xlabel('Frequency, Hz')
 ax.set_ylabel('Times transmitted')
-plt.xlim([10, 36])
+plt.xlim([9, 36])
 ax.set_title('Frequency spectrum of the transmission')
 plt.show()
